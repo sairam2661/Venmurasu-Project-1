38928f56aa7a39f4c678af76ebcb4fb6e537e406
sairam2661 <sairamvaidya2661@gmail.com>
2021-08-25 19:56:01 +0530

    Documetation
    Adding comments

--- data/english_cleanup.py
+++ data/english_cleanup.py
@@ -1,15 +1,23 @@
+'''Program to cleanup English files'''
+
 import re
 
+# Function to clean all lines in the files
+
 def cleanLine(line):
-  if ('p. ' in line): 
+  if ('p. ' in line):   # Removing page numbers 
     return ''
   else:
     x = ''
-    x = re.sub('\n', '', line)
+    x = re.sub('\n', '', line)    # General cleaning
     return x
+
+# Removing multiple spaces in final occurence
 
 def cleanPara(string):
   return re.sub(r'^.*?"', '"', string)  
+
+# Cleaning apostrophe's from paragraph
 
 def cleanApostrophe(string):
    line = string.split()
@@ -20,23 +28,29 @@ def cleanApostrophe(string):
        line[ele] = re.sub("'", '', line[ele])
    return " ".join(line)
 
+# Driver function
 if __name__ == "__main__":
   lst1 = ['resources/Mahabharatha-Adiparva-Section22-en.txt', 'resources/Mahabharatha-Adiparva-Section23-en.txt', 'resources/Mahabharatha-Adiparva-Section24-en.txt', 'resources/Mahabharatha-Adiparva-Section25-en.txt', 'resources/Mahabharatha-Adiparva-Section26-en.txt', 'resources/Mahabharatha-Adiparva-Section27-en.txt', 'resources/Mahabharatha-Adiparva-Section28-en.txt', 'resources/Mahabharatha-Adiparva-Section29-en.txt', 'resources/Mahabharatha-Adiparva-Section30-en.txt', 'resources/Mahabharatha-Adiparva-Section31-en.txt']
-  file1 = open('data/final_english.txt', 'w')
-  file1.close()
+  
+  fileFinal = open('data/final_english.txt', 'w')
+  fileFinal.close()
+  
   for file_name in lst1:
     file = open(file_name,'r')
     string = ""
 
     for line in file:  
-      string += cleanLine(line)
+      string += cleanLine(line)   # Cleaning line
 
-    string = cleanPara(string)
-    string = cleanApostrophe(string)
-    file1 = open('data/final_english.txt', 'a')
-    file1.write(string)
-    file1.close()
+    string = cleanPara(string)    # Cleaning paragraph
+    string = cleanApostrophe(string)  # Cleaning apostrophe
+    
+    fileFinal = open('data/final_english.txt', 'a') 
+    fileFinal.write(string) # Writing all lines to final_english.txt
+    
+    fileFinal.close()
+    
     file.close()
-  print("success")
+  
   
   
--- data/tamil_cleanup.py
+++ data/tamil_cleanup.py
@@ -1,19 +1,39 @@
+'''Program to cleanup Tamil files'''
+
 import re
-lst = ['resources/Mahabharatha-Adiparva-Section22-ta.txt', 'resources/Mahabharatha-Adiparva-Section23-ta.txt', 'resources/Mahabharatha-Adiparva-Section24-ta.txt', 'resources/Mahabharatha-Adiparva-Section25-ta.txt', 'resources/Mahabharatha-Adiparva-Section26-ta.txt', 'resources/Mahabharatha-Adiparva-Section27-ta.txt', 'resources/Mahabharatha-Adiparva-Section28-ta.txt', 'resources/Mahabharatha-Adiparva-Section29-ta.txt', 'resources/Mahabharatha-Adiparva-Section30-ta.txt', 'resources/Mahabharatha-Adiparva-Section31-ta.txt']
-file1 = open('data/final_tamil.txt', 'w', encoding='utf-8')
-file1.close()
-for file_name in lst:
-  file = open(file_name, encoding='utf-8')
-  string = ""
-  for line in file:
-      x = ''
-      x = re.sub('\n', '', line)
-      x = re.sub(''r"\([^()]*\)", "", x)
-      x = re.sub(''r"\[[^()]*\]", "", x)
-      x = re.sub(r'{.*?}', "", x, flags=re.UNICODE)
-      x = re.sub('"', '', x)
-      string += x
-  string = " ".join(string.split())
-  file1 = open('data/final_tamil.txt', 'a', encoding='utf-8')
-  file1.write(string)
-  file1.close()
+
+# Function to clean all lines in the files
+
+def cleanLine(line):
+  x = ''
+  x = re.sub('\n', '', line)
+  x = re.sub(''r"\([^()]*\)", "", x)
+  x = re.sub(''r"\[[^()]*\]", "", x)
+  x = re.sub(r'{.*?}', "", x, flags=re.UNICODE)
+  x = re.sub('"', '', x)
+  return x
+
+# Driver function
+
+if __name__ == '__main__':
+  
+  lst = ['resources/Mahabharatha-Adiparva-Section22-ta.txt', 'resources/Mahabharatha-Adiparva-Section23-ta.txt', 'resources/Mahabharatha-Adiparva-Section24-ta.txt', 'resources/Mahabharatha-Adiparva-Section25-ta.txt', 'resources/Mahabharatha-Adiparva-Section26-ta.txt', 'resources/Mahabharatha-Adiparva-Section27-ta.txt', 'resources/Mahabharatha-Adiparva-Section28-ta.txt', 'resources/Mahabharatha-Adiparva-Section29-ta.txt', 'resources/Mahabharatha-Adiparva-Section30-ta.txt', 'resources/Mahabharatha-Adiparva-Section31-ta.txt']
+  
+  fileFinal = open('data/final_tamil.txt', 'w', encoding='utf-8')
+  fileFinal.close()
+  
+  for file_name in lst:
+    file = open(file_name, encoding='utf-8')
+    string = ""
+    
+    for line in file:
+        x = cleanLine(line)
+        string += x
+    
+    string = " ".join(string.split())
+    
+    fileFinal = open('data/final_tamil.txt', 'a', encoding='utf-8')
+    fileFinal.write(string)
+    
+    fileFinal.close()
+    
